064c54424d1ed5616becdd78f936be2582087158
Victor Duarte <victor.d.dev@gmail.com>
2022-09-01 15:30:37 -0300

    added api request

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { Header } from "./components/Header"
-import { Home } from "./components/Home"
-import './main.css'
-import {Routes, Route} from 'react-router-dom'
-import { Pokedex } from "./components/Pokedex"
-import { Legendaries } from "./components/Legendaries"
-import { Documentation } from "./components/Documentation"
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+import "./main.css";
+import { Routes, Route } from "react-router-dom";
+import { Pokedex } from "./components/Pokedex";
+import { Legendaries } from "./components/Legendaries";
+import { Documentation } from "./components/Documentation";
 
 function App() {
-  return(
-      <div className="main">
-        <Header />
-        <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/pokedex" element={<Pokedex />} />
-                <Route path="/legendaries" element={<Legendaries />} />
-                <Route path="/Documentation" element={<Documentation />} />
-        </Routes>
-      </div>
-  )
+  return (
+    <div className="main">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+        <Route path="/legendaries" element={<Legendaries />} />
+        <Route path="/Documentation" element={<Documentation />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Pokedex/index.jsx
+++ src/components/Pokedex/index.jsx
@@ -1,5 +1,35 @@
-export function Pokedex(){
-    return(
-        <h1>pokedex</h1>
-    )
+import { useEffect, useState } from "react";
+
+export function Pokedex() {
+  const [pokedex, setPokedex] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
+
+  function getPokemons() {
+    pokedex.map((e) => {
+      fetch(e.url)
+        .then((res) => res.json())
+        .then((data) => setPokemon(data));
+    });
+  }
+  function getPokedex() {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((data) => setPokedex(data.results));
+    getPokemons();
+  }
+
+  useEffect(() => {
+    getPokedex();
+    getPokemons();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {pokedex.map((e) => {
+          return <li key={e.name}>{e.name}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
